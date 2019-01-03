70bca85f797999ee71997b126d9a976183140674
salido-m <salido-m@users.noreply.github.com>
2019-01-03 12:30:23 +0100

    Initial commit
    Creating solution, gang of four patterns project and adding basic singleton implementation

---
+++ GangOfFour/GangOfFour.Creational/Singleton.cs
@@ -0,0 +1,13 @@
+﻿namespace GangOfFour.Creational
+{
+    public class Singleton
+    {
+        private static readonly Singleton _instance = new Singleton();
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get { return _instance; }
+        }
+    }
+}
